93ba3cf376bbb6352351aed4e9337fd9807d0edf
Leandro Paiva <leandroapaiva@yahoo.com>
2022-01-22 17:39:56 -0300

    item 1

--- src/average.js
+++ src/average.js
@@ -11,14 +11,15 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+/* Neste exercício recebi ajuda do aluno Tiago Jerônimo Turma-19-C*/
 
-/* Neste exercício recebi ajuda do aluno Tiago Jerônimo */
 const average = (value1) => {
   if (value1.length === 0) {
   return (undefined);
 }
 
 let sum = 0;
+
 for (let index = 0; index < value1.length; index += 1) {
   const type = typeof value1[index];
   if (type !== 'number') {
@@ -29,4 +30,4 @@ const averageEnd = Math.round(sum / value1.length);
 return averageEnd;
 };
 
-exports = average;
+module.exports = average;
