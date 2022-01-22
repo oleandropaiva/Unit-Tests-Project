a44aa65e5d0cbe380dd5681e794b106e12511818
Leandro Paiva <leandroapaiva@yahoo.com>
2022-01-22 17:19:21 -0300

    item 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-//add your implamentation here
-module.exports = average;
+/* Neste exercício recebi ajuda do aluno Tiago Jerônimo */
+const average = (value1) => {
+  if (value1.length === 0) {
+  return (undefined);
+}
+
+let sum = 0;
+for (let index = 0; index < value1.length; index += 1) {
+  const type = typeof value1[index];
+  if (type !== 'number') {
+    return undefined;
+  } sum += value1[index];
+}
+const averageEnd = Math.round(sum / value1.length);
+return averageEnd;
+};
+
+exports = average;
