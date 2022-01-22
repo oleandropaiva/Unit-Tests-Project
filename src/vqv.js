03d1f11e98e1cc122bee4a6eb5bbd14389972300
Leandro Paiva <leandroapaiva@yahoo.com>
2022-01-22 19:52:05 -0300

    item 3

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,15 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
-const vqv = () => {};
+/* Neste exercício recebi ajuda do aluno Tiago Jerônimo Turma-19-C */
+const vqv = (name, age) => {
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
