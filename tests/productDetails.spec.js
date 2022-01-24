2e9c06c9340dbe8a97b6d0e398cdfeceb98d8a85
Leandro Paiva <leandroapaiva@yahoo.com>
2022-01-24 13:33:11 -0300

    item 6 teste

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -28,11 +29,27 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+
+    expect (typeof productDetails).toBe('function'); 
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+ /*    assert.strictEqual(typeof Object.values('Alcool gel', 'Máscara'),'object'); */
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[0],'object'); 
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[1],'object');
+    /* O método assert.deepStrictEqual() testa se dois objetos e seus objetos filhos são iguais, usando o operador ===. */
+    /* Se dois objetos, ou seus objetos filhos, não forem iguais (tanto em valor quanto em tipo), um erro será lançado e o programa será encerrado: */
+    /* fonte de estudos: https://www.w3schools.com/nodejs/met_assert_deepstrictequal.asp */
+
+
+
+
+
+
+
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
+    // Teste se productDetails é uma função. X
+    // Teste se o retorno da função é um array.X
+    // Teste se o array retornado pela função contém dois itens dentro.X
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
